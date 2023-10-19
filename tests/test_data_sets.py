98c76c3ca5cd7051246f80b4a7c6572d58863e28
Christoph Fink <christoph.fink@gmail.com>
2023-10-19 17:20:46 +0300

    fix tests

--- tests/test_data_sets.py
+++ tests/test_data_sets.py
@@ -7,5 +7,5 @@ import r5py.util
 class TestDataSets:
     def test_download_and_checksum(self, data_sets):
         for data_set in data_sets:
-            assert isinstance(data_set, r5py.util.data_set.DataSet)
+            assert isinstance(data_set, r5py.util.sample_data_set.SampleDataSet)
             assert data_set.exists()
